09a1b5e2639a1bcf0058b4eaac5b62eaea2741b0
calebishere <99222890+calebishere@users.noreply.github.com>
2022-03-16 09:51:47 +1300

    Added some more comments
    Going to fill these section in with code relavent to the comments name

---
+++ cpu_shop_main.py
@@ -0,0 +1,46 @@
+# Make a list of bot names
+
+
+
+# Ask user if order is for collect or order
+
+
+
+
+# List of CPU's to buy
+
+
+
+
+
+# order = delivery then ask for name - etc
+
+
+
+
+# Pickup order with name and phone number
+
+
+
+
+
+# Display the cost 
+
+
+
+
+
+# display the whole order when finished
+
+
+
+
+
+
+
+# be able to quit or continue order after completion
+
+
+
+def main():
+    
