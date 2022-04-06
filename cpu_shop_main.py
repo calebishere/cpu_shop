8d3dd48ab1ae8178c4450315acda7450c477c2d7
calebishere <99222890+calebishere@users.noreply.github.com>
2022-04-06 15:03:58 +1200

    updated menu a bit
    updated the menu by adding some code which allows the list of cpu's on ssale look more nice.

--- cpu_shop_main.py
+++ cpu_shop_main.py
@@ -1,3 +1,11 @@
+#CPU list and list of CPU prices
+
+cpu_list = ["i3-8100", "i3-9100", "i3-10400", "i3-1115GRE", "i5-8600", "i5-9400", "i5-10400", 
+            "i5-11400", "i7-8700", "i7-9700", "i7-10900", "i7-11800"]
+cpu_prices = [100.0, 120.0, 140.0, 160.0, 170.0, 190.0, 210.0, 220.0, 240.0, 255.0, 270.0, 300.0 ]
+
+
+
 # Cpu Shop
 customer_detail = {}
 def not_blank(question):
@@ -91,6 +99,15 @@ def deliv_info():
     customer_detail["suburb"] = not_blank(question)
     print (customer_detail["suburb"])
 
+# CPU list of sales
+def menu():
+    number_cpu = 12
+
+    for count in range(number_cpu):
+        print("{} {} ${}" .format(count+1,cpu_list[count],cpu_prices[count]))
+
+
+
 # Pickup order with name and phone number
 
 
@@ -178,5 +195,6 @@ def main():
     '''
     welcome_msg()
     pick_deli()
+    menu()
     
 main()
