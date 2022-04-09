ed21ff72916d033fbae05ff61646f8f472ae84d4
calebishere <99222890+calebishere@users.noreply.github.com>
2022-04-09 20:47:12 +1200

    intergrated my program on to the main
    intergrating the order into my main cpu program so now people can order cpus

--- Components/CPU_ordering_v2.py
+++ Components/CPU_ordering_v2.py
@@ -19,22 +19,42 @@ menu()
 
 # ask for the toal number of CPU's
 num_CPU = 0
-num_CPU = int(input("How many Cpus's would you like to get?: "))
+
+while True:
+    try:
+        num_CPU = int(input("How many Cpus's would you like to get?: "))
+        if num_CPU >= 1 and num_CPU <=5:
+            break
+        else:
+            print("Try and choose between 1 - 5") 
+        
+    except ValueError:
+        print("Please enter a number...")
+
 
 print(num_CPU)
 
 # Choose cpu from the list provided
-print("Please choose your CPU by entering the number from the menu")
 for item in range(num_CPU):
     while num_CPU > 0:
-        CPU_ordered = int(input())
+        while True:
+            try:
+                CPU_ordered = int(input("Please choose your CPU by entering the number from the menu: "))
+                if CPU_ordered >= 1 and CPU_ordered <=12:
+                    break
+                else:
+                    print("Try and choose between 1 - 12") 
+        
+            except ValueError:
+                print("Please enter a number...")
+
+        CPU_ordered = CPU_ordered -1
         order_list.append(cpu_list[CPU_ordered])
         order_cost.append(cpu_prices[CPU_ordered])
         print("{} ${}".format(cpu_list[CPU_ordered],cpu_prices[CPU_ordered  ]))
         num_CPU = num_CPU-1
         
-#print(order_list)
-#print(order_cost)
+print(order_list, order_cost)
 
 
 
--- cpu_shop_main.py
+++ cpu_shop_main.py
@@ -108,27 +108,58 @@ def menu():
 
 
 
-# Pickup order with name and phone number
+# choose total number of CPUs which is max of 5 CPUs 
+# List of CPUs
+
+cpu_list = ["i3-8100", "i3-9100", "i3-10400", "i3-1115GRE", "i5-8600", "i5-9400", "i5-10400", 
+            "i5-11400", "i7-8700", "i7-9700", "i7-10900", "i7-11800"]
+cpu_prices = [100.0, 120.0, 140.0, 160.0, 170.0, 190.0, 210.0, 220.0, 240.0, 255.0, 270.0, 300.0 ]
+
+# list to store ordered CPUs
+order_list = []
+# list to store cost of CPUs
+order_cost = []
+
+def menu():
+    number_cpu = 12
+
+    for count in range(number_cpu):
+        print("{} {} ${}" .format(count+1,cpu_list[count],cpu_prices[count]))
 
 
+def order_cpu():
+    # ask for the toal number of CPU's
+    num_CPU = 0
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    while True:
+        try:
+            num_CPU = int(input("How many Cpus's would you like to get?: "))
+            if num_CPU >= 1 and num_CPU <=5:
+                break
+            else:
+                print("Try and choose between 1 - 5") 
+        except ValueError:
+            print("Please enter a number...")
+    # Choose cpu from the list provided
+    for item in range(num_CPU):
+        while num_CPU > 0:
+            while True:
+                try:
+                    CPU_ordered = int(input("Please choose your CPU by entering the number from the menu: "))
+                    if CPU_ordered >= 1 and CPU_ordered <=12:
+                        break
+                    else:
+                        print("Try and choose between 1 - 12") 
+            
+                except ValueError:
+                    print("Please enter a number...")
+            CPU_ordered = CPU_ordered -1
+            order_list.append(cpu_list[CPU_ordered])
+            order_cost.append(cpu_prices[CPU_ordered])
+            print("{} ${}".format(cpu_list[CPU_ordered],cpu_prices[CPU_ordered  ]))
+            num_CPU = num_CPU-1
+            
+    print(order_list, order_cost)
 
 
 
@@ -196,5 +227,6 @@ def main():
     welcome_msg()
     pick_deli()
     menu()
+    order_cpu()
     
 main()
