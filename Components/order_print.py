57c16dd379dd351f50c364ad6e5176bdfb2eebb1
calebishere <99222890+calebishere@users.noreply.github.com>
2022-04-11 15:29:02 +1200

    further editing on printing order
    Added some more formatting for the user to be able to catch information a lot easier.

--- Components/order_print.py
+++ Components/order_print.py
@@ -4,7 +4,15 @@ order_list = ["i3-8100", "i7-9700"]
 # list to store cost of CPUs
 order_cost = [100.0, 255.0]
 
+customer_detail = {"name": "Caleb", "phone": "020202020", "house": "4", "street": "Fanglorish", "suburb": "Gargafrum"}
+
+
+#print("\n", customer_detail['name'],"\n", customer_detail["phone"], "\n", customer_detail["house"], "\n", customer_detail["street"], "\n", customer_detail["suburb"])
+
+
+print("\nCustomer Name: {} \nCustomer Phone: {} \nCustomer House: {} \nCustomer Street: {} \nCustomer Suburb: {}" .format(customer_detail['name'], customer_detail["phone"], customer_detail["house"], customer_detail["street"], customer_detail["suburb"]))
+
 count = 0
 for item in order_list:
-    print("{} ${}".format(item, order_cost[count]))
+    print("Ordered: {} ${}".format(item, order_cost[count]))
     count = count+1
